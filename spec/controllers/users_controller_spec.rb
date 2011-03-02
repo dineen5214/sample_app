c1ec5b26f75e63d82c429199e53cde1fab3f5175
Tom Dineen <dineen5214@gmail.com>
2011-03-02 18:22:53 -0500

    protecting edit and upate with before filter

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -40,7 +40,7 @@ describe UsersController do
       response.should   have_selector('td>a', :content => user_path(@user),
 					     :href  =>  user_path(@user))
     end
-    end
+  end
 
   describe "GET 'new'" do
     it "should be successful" do
@@ -107,6 +107,99 @@ describe UsersController do
         controller.should  be_signed_in
       end
 
+    end
+  end
+
+  describe "GET 'edit'" do
+
+    before(:each) do
+     # want users sign'ed in
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    it "should be successful" do
+     get :edit, :id => @user
+     response.should be_success
+    end
+
+    it "should have the right title" do
+     get :edit, :id => @user
+     response.should have_selector('title', :content => "Edit user")
+    end
+
+    it "should have a link to change the Gravatar" do
+      get :edit, :id => @user
+      response.should have_selector('a', :href => 'http://gravatar.com/emails',
+                                         :content => "change")
+    end
+  end
+
+  describe "PUT 'update'" do
+
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    describe "failure" do
+    
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "should render the 'edit' page" do 	# put us on the same page after hitting the Update btn
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit')
+
+      end
+
+      it "should have the right title" do
+       put :update, :id => @user, :user => @attr
+       response.should have_selector('title', :content => "Edit user")
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New Name", :email => "user@example.org", :password => "barbaz",
+                  :password_confirmation => "barbaz" }
+      end
+
+      it "should change the user's attributes" do
+        put :update, :id => @user, :user => @attr
+        user = assigns(:user)
+        @user.reload
+        @user.name.should == user.name
+        @user.email.should == user.email
+        @user.encrypted_password.should == user.encrypted_password
+      end
+
+      it "should have a flash message" do
+        put :update, :id => @user, :user => @attr
+        flash[:success].should =~ /updated/i
+      end
+
+    end
+  end
+
+  describe "authentication of edit/update actions" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should deny access to 'edit'" do
+      get :edit, :id => @user
+      response.should redirect_to(signin_path)
+      flash[:notice].should =~ /sign in/i
+    end
+
+    it "should deny access to 'update'" do
+      put :update, :id => @user, :user => {}
+      response.should redirect_to(signin_path)
     end
   end
 end
