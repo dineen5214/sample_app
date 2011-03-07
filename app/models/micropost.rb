18cd36d1db70964e05731db80ac64488ffb20c4b
Tom Dineen <dineen5214@gmail.com>
2011-03-07 15:17:25 -0500

    Micropost validataions

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -15,6 +15,8 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
-  # in SQL desending order is DESC
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :user_id, :presence => true
+
   default_scope :order => 'microposts.created_at DESC'
 end
